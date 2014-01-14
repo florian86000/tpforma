c0149890404402854971ccc42a951320f79bce78
MDero <maxime.derobillard@gmail.com>
2014-01-14 11:20:42 +0100

    Test de PDB et mise en forme de celle ci

--- tpforma/src/main/java/fr/gphy/m2/keuv/tpforma/App.java
+++ tpforma/src/main/java/fr/gphy/m2/keuv/tpforma/App.java
@@ -1,14 +1,17 @@
 package fr.gphy.m2.keuv.tpforma;
 
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World! Florian est un tard looool");
-        //TOATST
-        //FILS de méchant dero lol
+        PDBUrlBuilder.getFirstPdbUrl("P07900");
+        
+        
     }
 }
--- tpforma/src/main/java/fr/gphy/m2/keuv/tpforma/PDBUrlBuilder.java
+++ tpforma/src/main/java/fr/gphy/m2/keuv/tpforma/PDBUrlBuilder.java
@@ -16,18 +16,23 @@ import java.net.URL;
  */
 public class PDBUrlBuilder {
     public static URL getFirstPdbUrl(String uniprotId) throws IOException {
-        // TODO: gérer le proxy
+        // Gérer le proxy
         System.setProperty("http.proxyHost","cache.univpoitiers.fr");
         System.setProperty("http.proxyPort", "3128");
+        
         URL url = new URL("http://www.uniprot.org/uniprot/" + uniprotId + ".txt");
         BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-        // TODO: lire le fichier
+        
+        
+        // Lire le fichier
         String line;
         while ((line = in.readLine()) != null) {
-        if (line.matches("^DR\\s+PDB;.+$")) { in.close();
-        return new URL("http://www.ebi.ac.uk/pdbe/entry­files/pdb" + line.substring(10, 14).toLowerCase() + ".ent");
+            if (line.matches("^DR\\s+PDB;.+$")) { 
+                in.close();
+                return new URL("http://www.ebi.ac.uk/pdbe/entry­files/pdb" + line.substring(10, 14).toLowerCase() + ".ent");
+            }
         }
-        }
+        
         in.close();
         return null;
     }
