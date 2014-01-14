3c235c6e9ed5dd828602ee13585fad59d80ae71a
MDero <maxime.derobillard@gmail.com>
2014-01-14 10:34:41 +0100

    Création de la classe PDBUrlBuilder

---
+++ tpforma/src/main/java/fr/gphy/m2/keuv/tpforma/PDBUrlBuilder.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.gphy.m2.keuv.tpforma;
+
+/**
+ *
+ * @author
+ * MiniMax
+ */
+public class PDBUrlBuilder {
+    
+}
